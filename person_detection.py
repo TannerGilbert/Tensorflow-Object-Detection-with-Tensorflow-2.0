397eb5970df0c2b8d89249c5873716a8ade7bdd6
TannerGilbert <gilberttanner.work@gmail.com>
2020-09-01 10:22:16 +0200

    updated inference files so they can also be used with custom models

--- person_detection.py
+++ person_detection.py
@@ -19,19 +19,8 @@ utils_ops.tf = tf.compat.v1
 tf.gfile = tf.io.gfile
 
 
-def load_model(model_name):
-    base_url = 'http://download.tensorflow.org/models/object_detection/'
-    model_file = model_name + '.tar.gz'
-    model_dir = tf.keras.utils.get_file(
-        fname=model_name,
-        origin=base_url + model_file,
-        untar=True)
-
-    model_dir = pathlib.Path(model_dir) / "saved_model"
-
-    model = tf.saved_model.load(str(model_dir))
-    model = model.signatures['serving_default']
-
+def load_model(model_path):
+    model = tf.saved_model.load(model_path)
     return model
 
 
